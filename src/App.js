c1697c0327c313c450de97f12ee1df0df97a8027
paolaGoyes <paolainga18@gmail.com>
2021-08-05 09:44:12 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=3Df-MtLj8J0"
           target="_blank"
           rel="noopener noreferrer"
         >
